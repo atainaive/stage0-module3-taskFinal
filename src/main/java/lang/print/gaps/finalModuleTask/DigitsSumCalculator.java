d13d0a1f17731989d85435e1ca3fbfa1518942be
atainaive <atai.omorov@stud.vgtu.lt>
2022-06-24 16:26:23 +0300

    commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        int remainder = 0;
+        while (number != 0) {
+            remainder = number % 10;
+            sum = sum + remainder;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int revertedNumber = 0;
+        while(number != 0){
+            int remainder = number % 10;
+
+            revertedNumber = revertedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        System.out.println(revertedNumber);
     }
 }
